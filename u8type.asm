7cf013b7b8001f31d2028c89b599e50f22cb1a84
Chris Sawer <cjsawer@whitewillow.co.uk>
2024-11-23 23:35:51 +0000

    commonDefinitions.asm

--- u8type.asm
+++ u8type.asm
@@ -1,25 +1,6 @@
+INCLUDE "../commonDefinitions.asm"
+
 buildRom = FALSE
-
-osfind = &FFCE
-osbget = &FFD7
-osargs = &FFDA
-osrdch = &FFE0
-osasci = &FFE3
-osnewl = &FFE7
-oswrch = &FFEE
-
-FLAGS_UNIX_LE     =  &1
-FLAGS_BEEB_LE     =  &2
-FLAGS_DOS_LE      =  &4
-FLAGS_ASCII       =  &8
-FLAGS_UTF8_DOUBLE = &10
-FLAGS_UTF8_TRIPLE = &20
-FLAGS_UTF8_QUAD   = &40
-FLAGS_BINARY      = &80
-
-ASCII_LF    = &0A
-ASCII_CR    = &0D
-ASCII_SPACE = &20
 
 ORG &70
 \ NOTE these are not auto-initialised!
